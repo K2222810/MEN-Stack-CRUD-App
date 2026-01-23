26a7350d7a7b39ec335b12caa036c196537aa909
K2222810 <axelbaynes@gmail.com>
2026-01-23 12:58:51 +0000

    updated js file

--- public/images.js
+++ public/images.js
@@ -1,13 +1,19 @@
 // Available pizza images from assets folder
 const pizzaImages = [
-  { name: 'Margherita', path: '/assets/margherita.jpg' },
-  { name: 'Pepperoni', path: '/assets/pepperoni.jpg' },
-  { name: 'Vegetarian', path: '/assets/vegetarian.jpg' },
-  { name: 'Hawaiian', path: '/assets/hawaiian.jpg' },
-  { name: 'BBQ Chicken', path: '/assets/bbq-chicken.jpg' },
-  { name: 'Four Cheese', path: '/assets/four-cheese.jpg' },
-  { name: 'Meat Lovers', path: '/assets/meat-lovers.jpg' },
-  { name: 'Seafood', path: '/assets/seafood.jpg' },
+  { name: 'Argentinian Fugazza', path: '/assets/argentinian-fugazza-pizza.jpg' },
+  { name: 'Brazilian Pizza', path: '/assets/brazilian-pizza.jpg' },
+  { name: 'California Pizza', path: '/assets/california-pizza.jpg' },
+  { name: 'Canadian Quebecois', path: '/assets/canadian-quebecois-pizza.jpg' },
+  { name: 'Chicago Deep Dish', path: '/assets/chicago-deep-dish-pizza.jpg' },
+  { name: 'Detroit Pizza', path: '/assets/detroit-pizza.jpg' },
+  { name: 'Hawaiian Pizza', path: '/assets/hawaiian-pizza.jpg' },
+  { name: 'Miami Cuban Pizza', path: '/assets/miami-cuban-pizza.jpg' },
+  { name: 'Milan Style', path: '/assets/milan-style.jpg' },
+  { name: 'Neapolitan Pizza', path: '/assets/neapolitan-pizza.jpg' },
+  { name: 'New York Pizza', path: '/assets/new-york-pizza.jpg' },
+  { name: 'Roman Pizza', path: '/assets/roman-pizza.jpg' },
+  { name: 'Sicilian Pizza', path: '/assets/sicilian-pizza.jpg' },
+  { name: 'Thai Pizza', path: '/assets/thai-pizza.jpg' },
 ];
 
 function initImageSelector() {
--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.use('/users', usersController);
 app.use('/recipes', recipesController);
 app.use('/ingredients', ingredientsController);
 
-app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+ 
+//app.listen(port, () => {
+//  console.log(`The express app is ready on port ${port}!`);
+// });
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on http://0.0.0.0:3000');
 });
+
