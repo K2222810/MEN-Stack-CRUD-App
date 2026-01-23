8b3d3e9cf6946da879baaa10f92a22fcc15dbbee
K2222810 <axelbaynes@gmail.com>
2026-01-23 13:29:49 +0000

    updated js file

--- server.js
+++ server.js
@@ -57,11 +57,15 @@ app.use('/recipes', recipesController);
 app.use('/ingredients', ingredientsController);
 
  
-//app.listen(port, () => {
-//  console.log(`The express app is ready on port ${port}!`);
-// });
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+ });
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server running on http://0.0.0.0:3000');
+/*
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
-
+*/
